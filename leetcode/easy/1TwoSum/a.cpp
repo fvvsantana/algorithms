9b2b6b6292aa3d0a07fdd0e291ec5babd923104d
Flavio Santana <fvvsantana@gmail.com>
2020-03-26 12:24:43 -0300

    Leetcode 1TwoSum

--- leetcode/easy/1TwoSum/a.cpp
+++ leetcode/easy/1TwoSum/a.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans = {0,1};
+        vector<int> ans(2);
+        unordered_map<int, int> ht(nums.size());
+
+        // Generate hash table <value, index>
         for(int i=0; i < nums.size(); i++){
-            printf("%d\n", nums[i]);
+            ht.insert(pair<int, int>(nums[i], i));
         }
 
-        return nums;
+        for(int i=0; i < nums.size(); i++){
+            unordered_map<int,int>::iterator index = ht.find(target - nums[i]);
+            if(index != ht.end() && index->second != i){
+                ans[0] = i;
+                ans[1] = index->second;
+                break;
+            }
+        }
+
+        return ans;
     }
 };
 
 int main(){
-    Solution sol;
-    vector<int> ans;
-    vector<int> entry = {0,1,2};
-    ans = sol.twoSum(entry, 3);
+    //vector<int> entry = {0,1,2,3};
+    vector<int> entry = {3,2,4};
+    vector<int> ans = Solution().twoSum(entry, 6);
     printf("%d %d\n", ans[0], ans[1]);
     return 0;
 }
